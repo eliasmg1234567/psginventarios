28ab0003a9c68360a770c0dc3cb65d4687b8a293
eliasmg12345 <randyofgod666@gmail.com>
2021-07-06 12:04:44 -0400

    esq2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './psg/head/head.component';
+import { AdministracionMaterialesComponent } from './psg/administracion-materiales/administracion-materiales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadComponent
+    HeadComponent,
+    AdministracionMaterialesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/psg/administracion-materiales/administracion-materiales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-administracion-materiales',
+  templateUrl: './administracion-materiales.component.html',
+  styleUrls: ['./administracion-materiales.component.css']
+})
+export class AdministracionMaterialesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
